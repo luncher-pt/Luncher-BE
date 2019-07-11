57c211551c020b43e82569ee1bed62ffa5eb822c
Trip4077 <bj2336@email.vccs.edu>
2019-07-11 13:45:47 -0400

    POST /schools GET /schools/:id Tests Created

--- Routes/api-tests/schoolsRoutes.test.js
+++ Routes/api-tests/schoolsRoutes.test.js
@@ -3,6 +3,22 @@ const request = require('supertest');
 const server = require('../../Server/server');
 const db = require('../../data/dbConfig');
 
+const mockSchools = [
+    {
+        name: 'High School',
+        address: 'Some Street',
+        funds_required: 500,
+        funds_donated: 0,
+        admin_id: 1 
+    },
+    {
+        name: 'High School',
+        address: 'Some Street',
+        funds_donated: 0,
+        admin_id: 1 
+    },
+]
+
 describe('SCHOOLS ROUTES', () => {
    
     beforeEach(async () => {
@@ -27,6 +43,67 @@ describe('SCHOOLS ROUTES', () => {
             const res = await request(server).get('/schools');
 
             expect(res.body.length).toBe(5);
+        });
+    });
+
+    describe('POST /schools', () => {
+
+        it('should add a school to the database', async () => {
+            const res = await request(server)
+                                .post('/schools')
+                                .send(mockSchools[0]);
+
+            expect(res.body).toBe(1);
+        });
+
+        it('should return status 201 if succesful', async () => {
+            const res = await request(server)
+                                .post('/schools')
+                                .send(mockSchools[0]);
+
+            exepct(res.status).toBe(201);
+        });
+
+        it('should return status 406 if missing a field', async () => {
+            const res = await request(server)
+                                .post('/schools')
+                                .send(mockSchools[1]);
+
+            expect(res.status).toBe(406);
+        });
+
+        it('should return status 400 if duplicate entry or school already found', async () => {
+            await request(server)
+                    .post('/schools')
+                    .send(mockSchools[0]);
+
+            const res = await request(server)
+                                .post('/schools')
+                                .send(mockSchools[0]);
+
+            expect(res.status).toBe(400);
+        });
+    });
+
+    describe('GET /schools/:id', () => {
+
+        it('should return a single school', async () => {
+            const res = await request(server).get('/schools/1');
+
+            expect(res.body.length).toBe(1);
+            expet(Array.isArray(res.body)).toBe(true);
+        });
+
+        it('should return status 200 if successful', async () => {
+            const res = await request(server).get('/schools/1');
+
+            expect(res.status).toBe(200);
+        });
+
+        it('should return status 404 if no school found', async () => {
+            const res = await request(server).get('/schools/10');
+
+            expect(res.status).toBe(404);
         });
     });
 });
