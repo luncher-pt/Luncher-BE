0935630aabff3b179179b4434cdb1a547a263094
Trip4077 <bj2336@email.vccs.edu>
2019-07-12 09:43:04 -0400

    User Routes Tests Pass

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -35,4 +35,21 @@ router.put('/:id', (req, res) => {
    }
 });
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    if(isNaN(Number(id))) {
+        res.status(400).json({ message: 'No ID Found' });
+    } else {
+        Users.deleteUser(id)
+             .then(user => {
+                 user.error ? res.status(404).json({ error: 'No User Found' })
+                            : res.status(200).json(user);
+             })
+             .catch(err => {
+                 res.status(500).json(err);
+             });
+    }
+});
+
 module.exports = router;
