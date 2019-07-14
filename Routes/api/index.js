80c7551e84df21302d439aca0d66868c2ca3ab68
MrT3313 <rturgeon13@gmail.com>
2019-07-14 11:00:42 -0500

    loginRoute -> connected to DIR:api -> index.js & loginRoute.js added to DIR:api & loginRoute.test.js ATTEMPT added to DIR:api-tests

--- Routes/api/index.js
+++ Routes/api/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
 const register = require('./registerRoute')
+const login = require('./loginRoute')
 
 const userRoutes = require('./userRoutes');
 const schoolRoutes = require('./schoolsRoutes');
 
 router.use('/register', register)
+router.use('/login', login)
 
 router.use('/users', userRoutes);
 router.use('/schools', schoolRoutes);
---
+++ Routes/api/loginRoute.js
@@ -0,0 +1,73 @@
+// IMPORTS
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const secrets = require('../../secrets.js')
+
+// KNEX DB 
+DB_KNEX = require('../../data/dbConfig.js')
+
+// ROUTER
+const router = express.Router()
+
+// - GET - //
+router.get('/', async(req,res) => {
+    console.log('loginRoute GET/')
+    res.status(200).json( {message: 'GET/ -> in loginRoute'} )
+})
+// - POST - //
+/* Accepted Shape
+    {
+        "email": "STRING",
+        "password": "STRING",
+    }
+EXAMPLE
+    {
+        "email": "Bill@Billy.com",
+        "password": "TacoMan"
+    }
+*/
+router.post('/', async(req, res) => {
+    console.log('loginRoute POST/')
+    const {email, password } = req.body
+        console.log('email, password', email, password)
+    
+    DB_KNEX("users")
+        .where('email', email)
+            .first()
+            .then(user => {
+                console.log('user', user)
+
+                const pwVerification = bcrypt.compareSync(password, user.password)
+                if (user && pwVerification) {
+
+                    console.log('secrets', secrets)
+                    console.log('secrets.jwtSecret', secrets.jwtSecret)
+
+                    const token = jwt.sign(
+                        {
+                            id: user.id,
+                        },
+                        secrets.jwtSecret,
+                        {
+                            expiredIn: '1d'
+                        }
+                    )
+
+                    res.status(200).json({
+                        user: user.id,
+                        token
+                    })
+
+                } else {
+                    res.status(401).json( {error: 'Unable to Login'})
+                } 
+            })
+            .catch( err => {
+                res.status(500).json( { error: 'Unable to Login'})
+            })
+})
+// - PUT - //
+// - DEL - //
+
+module.exports = router
