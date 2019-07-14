27a20de63c8936c089b7e42c7c2d91cad6389606
MrT3313 <rturgeon13@gmail.com>
2019-07-14 10:57:29 -0500

    registerRoute -> connected to DIR:api -> index.js & registerRoute.js added to DIR:api & registerRoute.test.js ATTEMPT added to DIR:api-tests

--- Routes/api/index.js
+++ Routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const register = require('./registerRoute')
+
 const userRoutes = require('./userRoutes');
 const schoolRoutes = require('./schoolsRoutes');
+
+router.use('/register', register)
 
 router.use('/users', userRoutes);
 router.use('/schools', schoolRoutes);
---
+++ Routes/api/registerRoute.js
@@ -0,0 +1,54 @@
+// EXPRESS
+const express = require('express')
+
+// KNEX DB
+    const DB_KNEX = require('../../data/dbConfig.js')
+
+// ROUTER
+    const router = express.Router()
+
+// MIDDLEWARE
+    const pwHash = require('../../middleware/pwHash.js')
+
+// - GET - //
+    router.get('/', async(req,res) => {
+        console.log('registerRoute GET/')
+        res.status(200).json( {message: 'GET/ -> in registerRouter'} )
+    })
+// - POST - //
+    /* Accepted Shape
+        {
+            "name": "STRING",
+            "email": "STRING",
+            "password": "STRING",
+            "admin": BOOL,
+            "donations": INTEGER
+        }
+    EXAMPLE
+        {
+            "name": "Billy",
+            "email": "Bill@Billy.com",
+            "password": "TacoMan",
+            "admin": false,
+            "donations": 0
+        }
+    */
+   router.post('/', pwHash, async(req,res) => {
+       console.log('registerRouter POST/')
+    //    console.log('req.body', req.body)
+
+       DB_KNEX("users")
+        .insert(req.body)
+        .then( results => {
+            console.log('results ', results )
+            res.status(201).json( results )
+        })
+        .catch( err => {
+            res.status(422).json( {error: 'Unable to register new user'})
+        })
+   })
+// - PUT - //
+// - DEL - //
+
+// EXPORTS
+    module.exports = router
